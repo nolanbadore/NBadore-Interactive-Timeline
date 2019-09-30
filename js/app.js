b4bb07a009a7412911d47a85d222bd3bf1bf4de6
nolanbadore <nolanedits@gmail.com>
2019-09-30 13:21:18 -0400

    update 3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$(document).foundation()
+$('.nextPage a').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  // set speed to 700ms
+  $('html,body').animate({
+    scrollTop: top
+    },550);
+
+});
