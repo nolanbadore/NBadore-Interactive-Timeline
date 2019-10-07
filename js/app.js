a81edd5a34c3cc72dcff285e846f92106e4cb7df
nolanbadore <nolanedits@gmail.com>
2019-10-07 13:48:38 -0400

    animation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
-$('.nextPage a').click(function(event){
+$('.next').click(function(e){
   
-  // prevent defalt click behavior
-  // don't jump to content
-  event.preventDefault();
-    
-  // identify position of target
-  var target = $(this).attr('href');
+  // prevent link from jumping down the page
+  e.preventDefault();
+
+  // get reference to current content container
+  var parent = $(this).parents('div').attr('id');
+
+  // use parent to find out what the next one is
+  var next = $('#'+parent).next().attr('id');
+  
+  // save the name of the next container w a hashtag
+  var target = '#'+next;
+  
+  // identify the top position of the next container
   var top = $(target).offset().top;
 
-  // animate scroll to target
-  // set speed to 700ms
+  // send this info to function that slides the page
+  animBody(top);
+
+});
+
+function animBody(top){
   $('html,body').animate({
     scrollTop: top
     },550);
-
-});
+}
